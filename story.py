cc0a212022ae19a1844b3cdb6f7798727d9babf3
Ryan Hastie <231561729+ryanhastie5890@users.noreply.github.com>
2025-09-16 17:56:13 +0000

    Changed a print statement in the right path

--- story.py
+++ story.py
@@ -14,7 +14,8 @@ def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
 
 def right_path():
-    print("You walk right and encounter a talking squirrel who challenges you to a duel.")
+    print("You find an evil wizard who asks you to help him")
+    
 
 def center_path():
     print("You walk straight and start approaching the base of a mountain")
